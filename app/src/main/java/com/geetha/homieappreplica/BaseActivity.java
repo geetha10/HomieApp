4d258e44d47b49dde8d7602881e03889651f0180
Geetha Medisetty <geetha.medisetty@gmail.com>
2020-02-11 01:27:36 -0800

    Adding Auth, Login and Logout

---
+++ app/src/main/java/com/geetha/homieappreplica/BaseActivity.java
@@ -0,0 +1,4 @@
+package com.geetha.homieappreplica;
+
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/geetha/homieappreplica/CreateAccountActivity.java
@@ -0,0 +1,117 @@
+package com.geetha.homieappreplica;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class CreateUserActivity extends BaseActivity implements View.OnClickListener {
+
+    private static final String TAG = "Create User Activity :";
+
+    EditText uFirstNameET;
+    EditText uLastNameET;
+    EditText userIdET;
+    EditText emailET;
+    EditText passwordET;
+    Button createUserBT;
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createaccountform);
+        uFirstNameET=findViewById(R.id.firstNameEt);
+        uLastNameET=findViewById(R.id.lastNameET);
+        userIdET=findViewById(R.id.userIdET);
+        emailET=findViewById(R.id.emailET);
+        passwordET=findViewById(R.id.passwordET);
+        createUserBT=findViewById(R.id.createUserBT);
+        createUserBT.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    @Override
+    public void onClick(View v) {
+        String email=emailET.getText().toString();
+        String password=passwordET.getText().toString();
+        createAccount(email,password);
+
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        showProgressBar();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Toast.makeText(CreateUserActivity.this,"Account created for User : "+user,Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(CreateUserActivity.this, "Authentication failed." + task.getException().toString(),
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                        hideProgressBar();
+                    }
+                });
+
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = emailET.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailET.setError("Required.");
+            valid = false;
+        } else {
+            emailET.setError(null);
+        }
+
+        String password = passwordET.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            passwordET.setError("Required.");
+            valid = false;
+        } else {
+            passwordET.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressBar();
+        if (user != null) {
+
+            findViewById(R.id.userSignInBT).setVisibility(View.VISIBLE);
+
+        } else {
+
+            //findViewById(R.id.userSignInBT).setVisibility(View.GONE);
+        }
+    }
+}
